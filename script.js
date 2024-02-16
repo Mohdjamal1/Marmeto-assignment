b3e68751820774392f15ec46ffa3fd1c96c7782d
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2024-02-16 04:02:42 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+const url = "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.json";
+let product = {};
+let imageDiv = document.querySelector('.image-div');
+function getData() {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        product = data.product;
+        console.log(product);
+        const image =  product.images[1];
+        // imageDiv.innerHTML = `<img src=${image} class='my' alt="Item"/>`
+        console.log(image);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+   
+}
+getData();
+
+
+
+
