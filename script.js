ea936188506ba8c6a2983a9d38f625f0a5935ceb
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2024-02-16 12:18:28 +0000

    new commit

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const url = "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/singleProduct.json";
 let product = {};
-let imageDiv = document.querySelector('.image-div');
+let disc = document.querySelector('.discount');
 function getData() {
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
         product = data.product;
         console.log(product);
-        const image =  product.images[1];
-        // imageDiv.innerHTML = `<img src=${image} class='my' alt="Item"/>`
-        console.log(image);
+        
+
+        const discPercent = findDiscount(product);
+        disc.innerHTML = `${discPercent}% Off`;
+        
+        
     })
     .catch((err) => {
         console.log(err);
@@ -18,6 +21,12 @@ function getData() {
 }
 getData();
 
-
+function findDiscount(product){
+    let actualprice = Number(product.compare_at_price.slice(1));
+    let discountprice = Number(product.price.slice(1));
+    let discount = actualprice-discountprice;
+    let discPercent = parseInt((discount/ actualprice)*100);
+    return discPercent;
+}
 
 
